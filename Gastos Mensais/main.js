127cc9a42286e7cdc3a1ee4d97dc7b0825656d9a
Marcos <norbrj@gmail.com>
2022-03-28 20:34:56 -0300

    acertos

--- Gastos Mensais/main.js
+++ Gastos Mensais/main.js
@@ -1,9 +1,45 @@
-function parcelas() {
-  const parcelas = document.querySelector("valor");
-      
-  const mparcelamento = ["A vista",parcelas,"1x","2x","3x","4x","5x","6x","7x","8x","9x","10x","11x","12x"];
+document.querySelector("parcelamento").onfocus = function () {
+                        
+  var comboCidades = document.getElementById("parcelamento");
+  while (comboCidades.length) {
+      comboCidades.remove(0);
+      }
+                      
+  const parcelas = document.querySelector('#valor').value;
+  
+  const mparcelamento = ["A vista"];
+  for (let indice=2; indice < 13; indice++)
+  {       let valor = (parcelas/indice).toFixed(2);
+          let conteudo = indice < 10 ? `0${indice} x R$ ${valor}`:`${indice} x R$ ${valor}`;
+          mparcelamento.push(conteudo);
+  }
   const select4 = document.getElementById("parcelamento");
   for (let parcelamento of mparcelamento)
       select4.options[select4.options.length] = new Option(parcelamento);
-
 }
+
+
+//function setResultado (msg, isValid) {
+document.querySelector("parcelamento").onfocus = function () {
+
+const resultado = document.querySelector("parcelamento");
+    
+    function criaP () {
+      const p = document.createElement('li');
+      return p;
+    }
+
+    resultado.innerHTML = '';
+
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add('paragrafo-resultado');
+  } else {
+    p.classList.add('bad');
+  }
+
+  p.innerText = "teste";
+  resultado.appendChild(p);
+//}
+};
