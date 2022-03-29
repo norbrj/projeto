374ef19241155f62359091a9eeaed0fdab7f8899
Marcos <norbrj@gmail.com>
2022-03-29 14:51:41 -0300

    Melhorado

--- Gastos Mensais/main.js
+++ Gastos Mensais/main.js
@@ -1,15 +1,54 @@
-document.querySelector("parcelamento").onfocus = function () {
-                        
-  var comboCidades = document.getElementById("parcelamento");
-  while (comboCidades.length) {
-      comboCidades.remove(0);
-      }
-                      
-  const parcelas = document.querySelector('#valor').value;
+const valor = document.querySelector('#valor');
+const comboparcelamento = document.querySelector('#parcelamento');
+
+const mcategoria = ["Arthur","Agua","Mercado","Lazer","Carro","Feira","Fármacia","Agua","Padaria"];
+const mdono = ["Licia","Marcos"];
+const mpagamento = ["Original","Inter","C6","Nubank","Picpay"];
+
+function limpacombo () {
+  const comboparcelas = document.getElementById("parcelamento");
+  while (comboparcelas.length) {
+      comboparcelas.remove(0);
+  }
+}
+
+function criacombo(elemento, array) {
+
+  let selecao = document.getElementById(elemento);
+  for (let linha of array)
+      selecao.options[selecao.options.length] = new Option(linha);
+ 
+}
+
+criacombo("Categoria",mcategoria);
+criacombo("Dono",mdono);
+criacombo("pagamento",mpagamento);
+
+
+//let select1 = document.getElementById("Categoria");
+//for (let categoria of mcategoria)
+  //   select1.options[select1.options.length] = new Option(categoria);
+
+//const mdono = ["Licia","Marcos"];
+//let select2 = document.getElementById("Dono");
+//for (let Dono of mdono)
+  //   select2.options[select2.options.length] = new Option(Dono);
+
+//const mpagamento = ["Original","Inter","C6","Nubank","Picpay"];
+//let select3 = document.getElementById("pagamento");
+//for (let pagamento of mpagamento)
+//    select3.options[select3.options.length] = new Option(pagamento);
+
+
+function preenchecombo()
+{
+  limpacombo();
   
+  const parcelas = document.querySelector('#valor').value;
+
   const mparcelamento = ["A vista"];
   for (let indice=2; indice < 13; indice++)
-  {       let valor = (parcelas/indice).toFixed(2);
+  {       let valor = Number((parcelas/indice)).toFixed(2);
           let conteudo = indice < 10 ? `0${indice} x R$ ${valor}`:`${indice} x R$ ${valor}`;
           mparcelamento.push(conteudo);
   }
@@ -18,28 +57,8 @@ document.querySelector("parcelamento").onfocus = function () {
       select4.options[select4.options.length] = new Option(parcelamento);
 }
 
+valor.addEventListener('focusout', () => preenchecombo());
 
-//function setResultado (msg, isValid) {
-document.querySelector("parcelamento").onfocus = function () {
 
-const resultado = document.querySelector("parcelamento");
-    
-    function criaP () {
-      const p = document.createElement('li');
-      return p;
-    }
+//comboparcelamento.addEventListener('mousedown', () => preenchecombo());
 
-    resultado.innerHTML = '';
-
-  const p = criaP();
-
-  if (isValid) {
-    p.classList.add('paragrafo-resultado');
-  } else {
-    p.classList.add('bad');
-  }
-
-  p.innerText = "teste";
-  resultado.appendChild(p);
-//}
-};
